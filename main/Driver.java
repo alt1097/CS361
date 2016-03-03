d142d0a03c6e31d21d3a133ba369c68a65cbbbe8
alt1097 <thompson.alt@gmail.com>
2016-03-03 16:17:16 -0600

    Added Driver class

---
+++ main/Driver.java
@@ -0,0 +1,12 @@
+package main;
+
+import java.util.Scanner;
+
+public class Driver {
+	public void main(String[] args)	{
+		Scanner scanner;
+		Simulator simulator = new Simulator();
+		System.out.println("please enter the filename of the test data:");
+		
+	}
+}
